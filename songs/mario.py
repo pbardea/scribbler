5e97ef3c8d01b528106d8c30d1421e42839db8a0
pbardea <pbardea@gmail.com>
2014-09-24 20:53:37 -0400

    First commit

---
+++ songs/mario.py
@@ -0,0 +1,15 @@
+from myro import *
+
+init("/dev/tty.IPRE6-185826-DevB")
+
+beep(0.1,600)
+beep(0.1,600)
+beep(0.1,0)
+beep(0.1,600)
+beep(0.1,0)
+beep(0.1,500)
+beep(0.1,600)
+beep(0.1,0)
+beep(0.1,700)
+beep(0.3,0)
+beep(0.1,450)
