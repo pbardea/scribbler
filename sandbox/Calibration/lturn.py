e1fbc036091908e26e55bb40d99126a03d4c2316
pbardea <pbardea@gmail.com>
2014-10-02 13:20:59 -0400

    Added smoothTurn

--- sandbox/Calibration/lturn.py
+++ sandbox/Calibration/lturn.py
@@ -5,4 +5,9 @@ init("com7")
 def leftTurnAngle(angle):
 	turnLeft(0.3,3.0/90*angle)
 
+def smoothLeft(ltSpd, rtSpd, time):
+  while timeRemaining(time):
+    motors(ltSpd,rtSpd)
+  stop()
+
 leftTurnAngle(90)
