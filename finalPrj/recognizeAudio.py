2ed0485f88a42a73fea85ee5efa23f95d2c214c1
pbardea <pbardea@gmail.com>
2014-10-27 21:31:10 -0400

    Fixed recognizeAudio.py

--- finalPrj/recognizeAudio.py
+++ finalPrj/recognizeAudio.py
@@ -2,12 +2,14 @@ import speech_recognition as sr #import the speech recognition module
 
 def getAudio():
     r = sr.Recognizer() #initialize the recognizer
+    audioStr = "Nothing yet..."
     with sr.Microphone() as source: #Take the microphone as the audio source
         audio = r.listen(source) #listen until the first pause
 
-    try:
-        audioStr  = r.recognize(audio) #store audio in audioStr
-    except LookupError: #can't understand audio
-        print("Could not understand audio")
+        try:
+            audioStr  = r.recognize(audio) #store audio in audioStr
+        except LookupError: #can't understand audio
+            audioStr = "Could not understand audio"
+    return audioStr
 
-getAudio()
+print getAudio()
