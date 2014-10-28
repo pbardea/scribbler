619ea3fe8f169e734d430296f306349369150d59
scottw <scott.a.wiebe@gmail.com>
2014-10-27 22:45:29 -0400

    Uploaded test_circles.py
    Contains left and right turns for circles to be drawn by scribbler.

---
+++ finalPrj/test_circles.py
@@ -0,0 +1,18 @@
+#from myro import *
+#init("com7")
+#circle calibration
+def circle_right(a,b,x):
+    while timeRemaining(5.045335)
+        motors(1.0, -.27)
+    stop()
+
+def circle_left(a,b,x):
+    while timeRemaining(5.04335):
+        motors(-.27, 1.0)
+    stop()
+#suggest a realigning def using the light sensors
+#must cross a line before everymore
+
+#forward(1, .57)
+#^^^^^^^^^^^^^^^this is for one square
+#distance across
