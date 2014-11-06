7752e6313ee9d76ef056d68308c638cc1cc8f509
pbardea <pbardea@gmail.com>
2014-11-06 10:55:01 -0500

    Added redimentary voice recognition

--- finalPrj/recognizeAudio.py
+++ finalPrj/recognizeAudio.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr #import the speech recognition module
 
 def getAudio():
+    # r = sr.Recognizer(language = "en-US", key = "AIzaSyC0BY4MvU0DNvVkRuK0r9uSHtcl_SPdylI")
     r = sr.Recognizer() #initialize the recognizer
     audioStr = "Nothing yet..."
     with sr.Microphone() as source: #Take the microphone as the audio source
@@ -13,4 +14,47 @@ def getAudio():
 
     return audioStr
 
-print getAudio()
+def decypherAudio(spoken):
+    vertical = -99
+    horizontal = -99
+    spoken = spoken.rstrip().split()
+    topSyn = ['top','up','upper','highest','uppermost','zenith','climax']
+    botSyn = ['bottom','down','base','lowest','floor','bottommost']
+    leftSyn = ['left','leftmost']
+    rightSyn = ['right','rightmost']
+    for i in range(len(spoken)):
+        word = spoken[i]
+        if word in topSyn:
+            vertical = 1
+        elif word in botSyn:
+            vertical = -1
+        elif ((word == 'middle') or (word == 'between')) and horizontal != -99:
+            vertical = 0
+        elif word in leftSyn:
+            horizontal = -1
+        elif word in rightSyn:
+            horizontal = 1
+        elif ((word == 'middle') or (word == 'between')) and vertical != -99:
+            horizontal = 0
+        elif (word == 'center'):
+            vertical = 0
+            horizontal = 0
+        elif (word == 'middle') or (word == 'between'):
+            vertical = 0
+    return [horizontal,vertical]
+    
+spoken = getAudio()
+print spoken
+print """   
+             _________________
+             |     |    |    |
+             |-1,1 | 0,1|1,1 |
+             -----------------
+             |     |    |    |
+             |-1,0 | 0,0|1,0 |
+             -----------------
+             |     |    |    |
+             |-1,-1|0,-1|1,-1|
+             ----------------
+"""
+print decypherAudio(spoken)
