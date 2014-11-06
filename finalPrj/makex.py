28d700d6dd8543102e9d2282ca28c21ceb8dcef2
scottw <scott.a.wiebe@gmail.com>
2014-11-06 12:31:11 -0500

    Fixed makex.py
    Fixed makex to wait between movements, and to have a more accurate 45
    degree turn.

--- finalPrj/makex.py
+++ finalPrj/makex.py
@@ -1,13 +1,19 @@
-def makex():
-	threesixty(1, -1, 0.4175)
-	forward(1, 0.806)
-	threesixty(1, -1, 1.6478)
-	forward(1, 0.403)
-	threesixty(1, -1, 0.835)
-	forward(1, 0.403)
-	threesixty(1, -1, 1.6478)
-	forward(1, 0.806)
-	threesixty(1, -1, 0.4175)
-	stop()
-#run without values
-#might need to double check the 45 degree turn
+def makex2():
+    	threesixty(1, -1, 0.465)
+    	wait(.2)
+        forward(1, 0.806)
+        wait(.1)
+        backward(1, 0.403)
+        wait(.1)
+        threesixty(1, -1, 0.835)
+        wait(.1)
+        forward(1, 0.403)
+        wait(.1)
+        backward(1, 0.806)
+        wait(.2)
+        threesixty(-1, 1, 0.465)
+        stop()
+#########################################################
+# need to modify depending on where we want to draw x
+# and where we want the bot to finish
+# add a ccw or cw in function?
