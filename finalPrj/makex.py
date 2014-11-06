d7785dd507faad35da1f436863fedc5431d6fe0d
scottw <scott.a.wiebe@gmail.com>
2014-11-06 12:28:04 -0500

    Added line_stop, makex, and threesixty functions
    line_stop moves at user specified speed and stops when it sees a line.
    makex draws an X on the board. threesixty has times for 45, 90, 180 and
    360 degree turns at full speed.

---
+++ finalPrj/makex.py
@@ -0,0 +1,13 @@
+def makex():
+	threesixty(1, -1, 0.4175)
+	forward(1, 0.806)
+	threesixty(1, -1, 1.6478)
+	forward(1, 0.403)
+	threesixty(1, -1, 0.835)
+	forward(1, 0.403)
+	threesixty(1, -1, 1.6478)
+	forward(1, 0.806)
+	threesixty(1, -1, 0.4175)
+	stop()
+#run without values
+#might need to double check the 45 degree turn
