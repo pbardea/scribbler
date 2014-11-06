220a5c4745349e716998841d14d2bd1576c8ec3d
scottw <scott.a.wiebe@gmail.com>
2014-11-06 13:11:54 -0500

    Fixed test_cirles.py
    Changed the circle_left and circle_right function to start from the edge
    of the box and drive to the other edge of the box

--- finalPrj/test_circles.py
+++ finalPrj/test_circles.py
@@ -1,14 +1,18 @@
 #from myro import *
 #init("com7")
 #circle calibration
-def circle_right(a,b,x):
-    while timeRemaining(5.045335)
+def circle_right():
+    forward(1, 0.22)
+    while timeRemaining(5.045335):
         motors(1.0, -.27)
+    forward(1, 0.32)
     stop()
 
-def circle_left(a,b,x):
+def circle_left():
+    forward(1, 0.22)
     while timeRemaining(5.04335):
         motors(-.27, 1.0)
+    forward(1, 0.32)
     stop()
 #suggest a realigning def using the light sensors
 #must cross a line before everymore
