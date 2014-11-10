ab5053fb07d55efe7d906c74c09c015076b94ea0
pbardea <pbardea@gmail.com>
2014-11-09 20:15:27 -0500

    Text to board placement transition works for cases excluding complex commands involving middle top and bottom

--- finalPrj/recognizeAudio.py
+++ finalPrj/recognizeAudio.py
@@ -1,19 +1,23 @@
 import speech_recognition as sr #import the speech recognition module
 
-def getTextInput():
-
-def getAudio():
+def getAudio(tries):
     # r = sr.Recognizer(language = "en-US", key = "AIzaSyC0BY4MvU0DNvVkRuK0r9uSHtcl_SPdylI")
+    triesThreshold = 2
     r = sr.Recognizer() #initialize the recognizer
-    audioStr = "Nothing yet..."
+    audioSte  = "Nothing yet..."
+    print "Listening..."
     with sr.Microphone() as source: #Take the microphone as the audio source
         audio = r.listen(source) #listen until the first pause
 
     try:
         audioStr  = r.recognize(audio) #store audio in audioStr
     except LookupError: #can't understand audio
+        if tries < triesThreshold:
+            print "Sorry, didn't catch that. Please try again."
+            audiStr = getAudio(tries+1)
+        else:
+            audioStr = raw_input("Could not understand audio, please enter your command: ")
         # audioStr = "Could not understand audio"
-        audioStr = raw_input("Could not understand audio, please enter your command: ")
 
     return audioStr
 
@@ -31,33 +35,36 @@ def convertCoord(cartesian):
 def getMove(spoken,botPos):
     vertical = -99
     horizontal = -99
-    spoken = spoken.rstrip().split()
+    spoken = spoken.lower().rstrip().split()
     topSyn = ['top','up','upper','highest','uppermost','zenith','climax']
     botSyn = ['bottom','down','base','lowest','floor','bottommost']
     leftSyn = ['left','leftmost']
     rightSyn = ['right','rightmost']
+    midSyn = ['middle','between']
     for i in range(len(spoken)):
         word = spoken[i]
         if word in topSyn:
             vertical = 1
         elif word in botSyn:
             vertical = -1
-        elif ((word == 'middle') or (word == 'between')) and horizontal != -99:
+        elif word in midSyn and horizontal != -99:
             vertical = 0
         elif word in leftSyn:
             horizontal = -1
         elif word in rightSyn:
             horizontal = 1
-        elif ((word == 'middle') or (word == 'between')) and vertical != -99:
+        elif word in midSyn and vertical != -99:
             horizontal = 0
-        elif (word == 'centre'):
+        elif word in midSyn:
+            vertical = 0
+        elif word == 'centre':
             vertical = 0
             horizontal = 0
-        elif (word == 'middle') or (word == 'between'):
-            vertical = 0
+        # print word, horizontal, vertical
     return [horizontal,vertical]
     
-spoken = getAudio()
+spoken = getAudio(0)
+# spoken = raw_input(":")
 print spoken
 print """   
              _________________
@@ -71,4 +78,4 @@ print """
              |-1,-1|0,-1|1,-1|
              ----------------
 """
-print getMove(spoken,[)
+print getMove(spoken,[-1,1])
