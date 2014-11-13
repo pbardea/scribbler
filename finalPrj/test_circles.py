4ba5d4702761bcd2240111c2f94d80c5e6182280
scottw <scott.a.wiebe@gmail.com>
2014-11-13 12:53:21 -0500

    Changed test_circles.py
    Fixed the circles so that the robot returns to the same position after
    drawing the circle. Also remains in the same orientation.

--- finalPrj/test_circles.py
+++ finalPrj/test_circles.py
@@ -5,14 +5,14 @@ def circle_right():
     forward(1, 0.22)
     while timeRemaining(5.045335):
         motors(1.0, -.27)
-    forward(1, 0.32)
+    backward(1, 0.32)
     stop()
 
 def circle_left():
     forward(1, 0.22)
     while timeRemaining(5.04335):
         motors(-.27, 1.0)
-    forward(1, 0.32)
+    backward(1, 0.32)
     stop()
 #suggest a realigning def using the light sensors
 #must cross a line before everymore
