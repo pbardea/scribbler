4c074f368e7c02c50c4a30429bd3c97f60e25a47
pbardea <pbardea@gmail.com>
2014-11-13 12:37:15 -0500

    Added basic relative motion and expanded thesaurus

--- finalPrj/recognizeAudio.py
+++ finalPrj/recognizeAudio.py
@@ -1,20 +1,29 @@
+"""
+Used to read in speech input and convert it to a action represented by numeric output.
+"""
 import speech_recognition as sr #import the speech recognition module
+
+#global thesaurus
+thesaurus = {'top':['top','up','upper','highest','uppermost','zenith','climax','above'], 'bottom':['bottom','down','base','lowest','floor','bottommost','below','under','underneath'],'left':['left','leftmost'],'right':['right','rightmost'],'middle':['middle','between'],'centre':['centre'],'bot':{'robot','bot','theo','mario'}}
 
 def getAudio(tries):
     # r = sr.Recognizer(language = "en-US", key = "AIzaSyC0BY4MvU0DNvVkRuK0r9uSHtcl_SPdylI")
     triesThreshold = 2
     r = sr.Recognizer() #initialize the recognizer
-    audioSte  = "Nothing yet..."
+    audioStr  = "Nothing yet..."
     print "Listening..."
     with sr.Microphone() as source: #Take the microphone as the audio source
         audio = r.listen(source) #listen until the first pause
+
+    r.pause_threshold = 0.8
+    r.energy_threshold = 200
 
     try:
         audioStr  = r.recognize(audio) #store audio in audioStr
     except LookupError: #can't understand audio
         if tries < triesThreshold:
             print "Sorry, didn't catch that. Please try again."
-            audiStr = getAudio(tries+1)
+            audioStr = getAudio(tries+1)
         else:
             audioStr = raw_input("Could not understand audio, please enter your command: ")
         # audioStr = "Could not understand audio"
@@ -32,35 +41,60 @@ def convertCoord(cartesian):
                 arrayCoord = j*3+i%3
     return arrayCoord
 
+def wordToInt(word):
+  convert = ['zero','one','two','three','four','five','six','seven','eight','nine']
+  #array serach function
+
+def relativeMotion(statement, botPos):#if a robot keyword is triggered, the last 10 words is passed back for decomposition
+  #define botPos as a cartesian system with the bottom left corner of the centre cell as 0,0. Assuming staying on corners of centre cell
+  horizontal = botPos[0]
+  vertical = botPos[1]
+  lastInt = 1 #default, so if user says 'left', it will move one left of the robot by deafult
+  for word in statement:
+    if wordToInt(word) >= 0: #then a number was spoken
+      lastInt = wordToInt(word)
+    else: #process as a word
+      if word in thesaurus['top']:
+        vertical += lastInt
+      elif word in thesaurus['bottom']:
+        vertical -= lastInt
+      elif word in thesaurus['left']:
+        horizontal -= lastInt
+      elif word in thesaurus['right']:
+        horizontal += lastInt
+  return [horizontal,vertical]
+
+
 def getMove(spoken,botPos):
     vertical = -99
     horizontal = -99
     spoken = spoken.lower().rstrip().split()
-    topSyn = ['top','up','upper','highest','uppermost','zenith','climax']
-    botSyn = ['bottom','down','base','lowest','floor','bottommost']
-    leftSyn = ['left','leftmost']
-    rightSyn = ['right','rightmost']
-    midSyn = ['middle','between']
     for i in range(len(spoken)):
         word = spoken[i]
-        if word in topSyn:
+        if word in thesaurus['top']:
             vertical = 1
-        elif word in botSyn:
+        elif word in thesaurus['bottom']:
             vertical = -1
-        elif word in midSyn and horizontal != -99:
+        elif word in thesaurus['middle'] and horizontal != -99:
             vertical = 0
-        elif word in leftSyn:
+        elif word in thesaurus['left']:
             horizontal = -1
-        elif word in rightSyn:
+        elif word in thesaurus['right']:
             horizontal = 1
-        elif word in midSyn and vertical != -99:
+        elif word in thesaurus['middle'] and vertical != -99:
             horizontal = 0
-        elif word in midSyn:
+        elif word in thesaurus['middle']:
             vertical = 0
-        elif word == 'centre':
+        elif word == thesaurus['centre']:
             vertical = 0
             horizontal = 0
-        # print word, horizontal, vertical
+        elif word in thesaurus['bot']:
+            print "Calling relative motion"
+            newPos = relativeMotion(spoken,[-0.5,0.5]) 
+            horizontal = newPos[0]
+            vertical = newPos[1]
+            break
+        print word, horizontal, vertical
     return [horizontal,vertical]
     
 spoken = getAudio(0)
