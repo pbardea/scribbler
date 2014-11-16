c75da81802599aaf7cdb3732990cf1e3ce134070
scottw <scott.a.wiebe@gmail.com>
2014-11-15 21:16:10 -0500

    Added draw_l.py
    Note: it's a lowercase L not a one.
    Started function to draw a 1 in the square. We are scrapping the X
    because of too many highly inaccurate turns.

---
+++ finalPrj/draw_l.py
@@ -0,0 +1,44 @@
+#use c=1, d=-1 or vise versa
+# 45 degree turn = 0.438
+# 90 degree turn = 0.835
+# 180 degree turn = 1.6478
+# 360 degree turn = 3.2835
+#at full speed for all times
+
+def draw_l(ud):#it's a little L not a one
+    if(lr=="up"):
+        threesixty(1, -1, 0.835)
+        wait(.2)
+        forward(1, 0.4)
+        wait(.2)
+        threesixty(1, -1, 0.835)
+        wait(.2)
+        forward(1, 0.8)
+        wait(.2)
+        backward(1, 0.8)
+        wait(.2)
+        threesixty(-1, 1, 0.835)
+        wait(.2)
+        forward(1, 0.4)
+        wait(.2)
+        threesixty(1, -1, 0.835)
+
+    if(lr=="down"):
+        threesixty(1, -1, 0.835)
+        wait(.2)
+        forward(1, 0.4)
+        wait(.2)
+        threesixty(1, -1, 0.835)
+        wait(.2)
+        forward(1, 0.8)
+        wait(.2)
+        backward(1, 0.8)
+        wait(.2)
+        threesixty(-1, 1, 0.835)
+        wait(.2)
+        forward(1, 0.4)
+        wait(.2)
+        threesixty(1, -1, 0.835)
+
+##everything here is for going to the right so far
+##both of these are the same!
